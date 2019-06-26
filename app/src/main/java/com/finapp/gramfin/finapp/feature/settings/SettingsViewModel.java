26c06d3fd25e7f59d1d29b93848d8e159f0d011c
antropit <antropit@hotmail.com>
2019-06-26 13:47:15 +0300

    fix Story 15: settings layout two-way data binding

--- app/src/main/java/com/finapp/gramfin/finapp/feature/settings/SettingsViewModel.java
+++ app/src/main/java/com/finapp/gramfin/finapp/feature/settings/SettingsViewModel.java
@@ -2,14 +2,20 @@ package com.finapp.gramfin.finapp.feature.settings;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.databinding.Bindable;
+import androidx.databinding.Observable;
+import androidx.databinding.PropertyChangeRegistry;
 import androidx.lifecycle.ViewModel;
 
+import com.finapp.gramfin.finapp.BR;
 import com.finapp.gramfin.finapp.R;
 import com.finapp.gramfin.finapp.feature.settings.model.SettingsModel;
 import com.finapp.gramfin.finapp.frag_router.FragmentRouter;
 
-class SettingsViewModel extends ViewModel {
-    private SettingsModel settingsModel;
+public class SettingsViewModel extends ViewModel implements Observable {
+    private PropertyChangeRegistry registry = new PropertyChangeRegistry();
+
+    @Nullable private SettingsModel settingsModel;
     @Nullable private AlertDialog alertDialog;
 
     void setupModel(AlertDialog alertDialog) {
@@ -21,11 +27,12 @@ class SettingsViewModel extends ViewModel {
                 FragmentRouter.getInstance().getString(R.string.password_holder));
     }
 
-    String getFullName() { return settingsModel.getFull_name(); }
-    String getEmail() { return settingsModel.getEmail(); }
-    String getPassword() { return settingsModel.getPassword(); }
+    @Nullable SettingsModel getSettingsModel() { return  settingsModel; }
+    @Bindable public String getFullName() { return settingsModel.getFull_name(); }
+    @Bindable public String getEmail() { return settingsModel.getEmail(); }
+    @Bindable public String getPassword() { return FragmentRouter.getInstance().getString(R.string.password_holder); }
 
-    void setResetState(boolean resetIsChecked) { settingsModel.setResetIsChecked(resetIsChecked); }
+    public void setResetState(boolean resetIsChecked) { settingsModel.setResetIsChecked(resetIsChecked); }
 
     void saveSettings(String full_name_new, String email_new, String password_new) {
         //TODO save settings
@@ -35,17 +42,29 @@ class SettingsViewModel extends ViewModel {
 
         if (settingsModel.isResetIsChecked()) { if (alertDialog != null) alertDialog.show(); }
         else { FragmentRouter.getInstance().notImplementedToast(); }
+
+        registry.notifyChange(this, BR._all);
     }
 
-    void resetStatistics() {
+    public void resetStatistics() {
         //TODO implement resetStatistics action
         if (alertDialog != null) { alertDialog.dismiss(); }
         FragmentRouter.getInstance().notImplementedToast();
     }
 
-    void cancelResetStatistics() {
+    public void cancelResetStatistics() {
         //TODO implement cancel resetStatistics action
         if (alertDialog != null) { alertDialog.cancel(); }
         FragmentRouter.getInstance().notImplementedToast();
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        registry.add(callback);
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        registry.remove(callback);
     }
 }
