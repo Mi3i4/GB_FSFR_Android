20d6c744f519abf60ebe3740ebf7757a9b836e06
antropit <antropit@hotmail.com>
2019-08-31 23:47:18 +0300

    fix Story 23: main menu adaptive item

--- app/src/main/java/com/finapp/gramfin/finapp/feature/main_menu_fragment/MainMenuFragment.java
+++ app/src/main/java/com/finapp/gramfin/finapp/feature/main_menu_fragment/MainMenuFragment.java
@@ -1,12 +1,16 @@
 package com.finapp.gramfin.finapp.feature.main_menu_fragment;
 
-import androidx.lifecycle.ViewModelProviders;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +25,8 @@ import com.finapp.gramfin.finapp.feature.main_menu_fragment.model.ModelMainMenuI
 public class MainMenuFragment extends Fragment {
 
     private MainMenuViewModel viewModel;
+    private Point screenSize = new Point();
+    private int LOW_SCREEN_HEIGHT = 1280;
 
     public static MainMenuFragment newInstance() {
         return new MainMenuFragment();
@@ -38,18 +44,20 @@ public class MainMenuFragment extends Fragment {
 
         getActivity().setTitle(R.string.main_menu_title);
 
-        viewModel = ViewModelProviders.of(this).get(MainMenuViewModel.class);
+        viewModel = new ViewModelProvider(this).get(MainMenuViewModel.class);
         viewModel.setupModel();
 
         ListView listView = getView().findViewById(R.id.lvMainMenu);
         ArrayAdapter<ModelMainMenuItem> adapter = new MainMenuAdapter(getActivity());
         listView.setAdapter(adapter);
 
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
+        display.getSize(screenSize);
     }
 
     private class MainMenuAdapter extends ArrayAdapter<ModelMainMenuItem> {
 
-        public MainMenuAdapter(@NonNull Context context) {
+        MainMenuAdapter(@NonNull Context context) {
             super(context, R.layout.main_menu_item, viewModel.getListMainMenu());
         }
 
@@ -61,10 +69,16 @@ public class MainMenuFragment extends Fragment {
                 convertView = LayoutInflater.from(getContext())
                         .inflate(R.layout.main_menu_item, null);
             }
-            ((ImageView) convertView.findViewById(R.id.main_menu_btn_img))
-                    .setImageResource(mainMenuItem.getImage());
+
             ((TextView) convertView.findViewById(R.id.main_menu_btn_title))
                     .setText(mainMenuItem.getTitle());
+
+            ImageView btnImage = convertView.findViewById(R.id.main_menu_btn_img);
+            btnImage.setImageResource(mainMenuItem.getImage());
+
+            if (screenSize.y <= LOW_SCREEN_HEIGHT) {
+                btnImage.getLayoutParams().height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
+            }
 
             convertView.setOnClickListener(mainMenuItem.getListener());
 
